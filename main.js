b3fa4aac55e396543c9ac3c4e5b754322fff9735
StefanGhiban <stefanghiban962@gmail.com>
2021-12-15 15:47:15 +0200

    lesson 5

--- main.js
+++ main.js
@@ -4,7 +4,16 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {
+                    id: 2234, color: 'green'
+                },
+                {
+                    id: 2234, color: 'blue'
+                }
+            ],
+            sizes: ['xl', 'l', 'm', 's', 'xs']
         }
     }
 })
